932b1cd0e85ecf4e204d9fcb21e5a4d0c769a6b2
Michael Salmon <msalmonse@gmail.com>
2019-08-29 10:12:56 +0200

    Initial Commit

---
+++ Cardiac/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cardiac
+//
+//  Created by Michael Salmon on 2019-08-29.
+//  Copyright © 2019 mesme. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
