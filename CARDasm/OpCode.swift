676a5adc7d528099cd4f3a6e7ee67f9cefe6064f
Michael Salmon <msalmonse@gmail.com>
2019-09-11 11:19:41 +0200

    All operations parsed Improved label handling More tests

--- CARDasm/OpCode.swift
+++ CARDasm/OpCode.swift
@@ -8,11 +8,30 @@
 
 import Foundation
 
-struct Location {
+class Location {
     var label: String? = nil
     var address: Int? = nil
     var used: [Int] = []
+
+    init(_ sub: Substring) {
+        // check for numeric argument
+        switch addressValue(sub) {
+        case let .success(addr): address = addr
+        case .failure:
+            // Has location already been defined?
+            label = String(sub)
+            locations[label!] = self
+        }
+    }
+
+    static func get(_ sub: Substring) -> Location {
+        let label = String(sub)
+        if let location = locations[label] { return location }
+        return Location(sub)
+    }
 }
+
+var locations = [String: Location]()
 
 enum OpCode {
     case inp(Location)
@@ -26,4 +45,35 @@ enum OpCode {
     case jmp(Location)
     case hrs(Location)
     case unk(Location)
+}
+
+fileprivate func shiftVal(_ sub: Substring)  -> Result<Int, Error> {
+    guard let value = Int(sub) else { return .failure(TokenError.invalidNumber) }
+    if !(0...9).contains(value) { return .failure(TokenError.shiftOutOfRange(value)) }
+    return .success(value)
+}
+
+func opCodeToken(_ words: [Substring]) -> Result<OpCode, Error> {
+    switch words[0] {
+    case "inp": return .success(.inp(Location.get(words[1])))
+    case "cla": return .success(.cla(Location.get(words[1])))
+    case "add": return .success(.add(Location.get(words[1])))
+    case "tac": return .success(.tac(Location.get(words[1])))
+    case "slr":
+        var sl = 0
+        switch shiftVal(words[1]) {
+        case let .success(slval): sl = slval
+        case let .failure(err): return .failure(err)
+        }
+        switch shiftVal(words[2]) {
+        case let .success(srval): return .success(.slr(sl, srval))
+        case let .failure(err): return .failure(err)
+        }
+    case "out": return .success(.out(Location.get(words[1])))
+    case "sto": return .success(.sto(Location.get(words[1])))
+    case "sub": return .success(.sub(Location.get(words[1])))
+    case "jmp": return .success(.jmp(Location.get(words[1])))
+    case "hrs": return .success(.hrs(Location.get(words[1])))
+    default: return .failure(TokenError.unknownOperation(String(words[0])))
+    }
 }
--- CARDasm/Tokenizer.swift
+++ CARDasm/Tokenizer.swift
@@ -11,35 +11,79 @@ import Foundation
 enum Tokens {
     case identifier(String)
     case number(String)
-    case data(String?, String)
-    case opCode(String?, OpCode)
-    case location(String)
+    case data(Location?, String)
+    case opCode(Location?, OpCode)
+    case location(Location?, Int)
+    case error(Int, Error)
 }
+
+enum TokenError: Error {
+    case addressOutOfRange(Int)
+    case invalidNumber
+    case redefinedLabel(String)
+    case shiftOutOfRange(Int)
+    case unknownOperation(String)
+    case wrongNumberOfArguments
+}
+
+func addressValue(_ sub: Substring) -> Result<Int, Error> {
+    guard let value = Int(sub) else { return .failure(TokenError.invalidNumber) }
+    if !(0...99).contains(value) { return .failure(TokenError.addressOutOfRange(value)) }
+    return .success(value)
+}
+
+fileprivate let expectedWords: [Substring: Int] = [
+    "slr":  3
+]
 
 func tokenize(_ indata: String) -> [Tokens] {
     var tokens: [Tokens] = []
     var lineData = indata
 
+    var lineCount = 0
+    var lineAddress = 2
     var lineStart = lineData.startIndex
     while lineStart < lineData.endIndex {
         let lineEnd = lineData.firstIndex(of: "\n") ?? lineData.endIndex
         let lineStop = lineData.firstIndex(of: "#") ?? lineEnd  // Remove comments
         let line = lineData[lineStart..<min(lineStop, lineEnd)]
+        lineCount += 1
         var words = line.split(separator: " ")
 
         if words.count > 0 {
-            var label: String? = nil
+            lineAddress += 1
+
+            var label: Location? = nil
             // Test for label
             if words[0].hasSuffix(":") {
-                label = String(words[0].dropLast(1))
+                label = Location.get(words[0].dropLast(1))
                 words.removeFirst()
             }
 
-            switch words[0] {
-            case "dat":
-                tokens.append(.data(label, String(words[1])))
-            default:
-                break
+            if label != nil && label!.address != nil {
+                // redefined label
+                tokens.append(.error(lineCount, TokenError.redefinedLabel(label!.label!)))
+            } else if words.count != expectedWords[words[0]] ?? 2 {
+                tokens.append(.error(lineCount, TokenError.wrongNumberOfArguments))
+            } else {
+                label?.address = lineAddress
+                switch words[0] {
+                case "dat": tokens.append(.data(label, String(words[1])))
+                case "loc":
+                    switch addressValue(words[1]) {
+                    case let .success(addr):
+                        lineAddress = addr
+                        label?.address = addr
+                        tokens.append(.location(label, addr))
+                    case let .failure(err):
+                        tokens.append(.error(lineCount, err))
+                    }
+                default:
+                    switch opCodeToken(words) {
+                    case let .success(opCode): tokens.append(.opCode(label, opCode))
+                    case let .failure(err): tokens.append(.error(lineCount, err))
+                    }
+                }
             }
         }
 
