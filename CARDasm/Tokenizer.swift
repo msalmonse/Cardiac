ef8efe36826c2edd0b69dcabdfa3d7b4bbad864b
Michael Salmon <msalmonse@gmail.com>
2019-09-11 08:51:05 +0200

    Start of tokenizer

--- CARDasm/Tokenizer.swift
+++ CARDasm/Tokenizer.swift
@@ -11,13 +11,42 @@ import Foundation
 enum Tokens {
     case identifier(String)
     case number(String)
-    case data
-    case opCode(OpCode)
+    case data(String?, String)
+    case opCode(String?, OpCode)
     case location(String)
 }
 
 func tokenize(_ indata: String) -> [Tokens] {
     var tokens: [Tokens] = []
+    var lineData = indata
+
+    var lineStart = lineData.startIndex
+    while lineStart < lineData.endIndex {
+        let lineEnd = lineData.firstIndex(of: "\n") ?? lineData.endIndex
+        let lineStop = lineData.firstIndex(of: "#") ?? lineEnd  // Remove comments
+        let line = lineData[lineStart..<min(lineStop, lineEnd)]
+        var words = line.split(separator: " ")
+
+        if words.count > 0 {
+            var label: String? = nil
+            // Test for label
+            if words[0].hasSuffix(":") {
+                label = String(words[0].dropLast(1))
+                words.removeFirst()
+            }
+
+            switch words[0] {
+            case "dat":
+                tokens.append(.data(label, String(words[1])))
+            default:
+                break
+            }
+        }
+
+        // move onto next line
+        lineData.removeSubrange(lineStart...lineEnd)
+        lineStart = lineData.startIndex
+    }
 
     return tokens
 }
